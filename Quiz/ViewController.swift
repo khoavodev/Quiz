152959a2f6fd70a5c5e9d0c0c370a045335b025d
kt <kt@kts-Mac.local>
2018-11-17 20:44:44 +0700

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by kt on 11/17/18.
+//  Copyright © 2018 Team Pirates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
